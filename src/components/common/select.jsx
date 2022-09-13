197542df03aed387ffa80eef53b899e415d0a8bc
BrianKiarieMwaniki <briank.mwaniki@gmail.com>
2022-09-13 18:53:02 +0300

    add bootstrap and select component

---
+++ src/components/common/select.jsx
@@ -0,0 +1,18 @@
+const Select = ({ name, label, options, error, ...rest }) => {
+  return (
+    <div className="form-group">
+      <label htmlFor={name}>{label}</label>
+      <select className="form-control" name={name} id={name} {...rest}>
+        <option value="" />
+        {options.map((option) => (
+          <option key={option} value={option}>
+            {option}
+          </option>
+        ))}
+      </select>
+      {error && <div className="alert alert-danger">{error}</div>}
+    </div>
+  );
+};
+
+export default Select;
