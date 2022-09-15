6668b4dddaf1fda80171ecbca5d12583f658a17f
BrianKiarieMwaniki <briank.mwaniki@gmail.com>
2022-09-15 08:50:15 +0300

    improve grid layout

--- src/components/common/select.jsx
+++ src/components/common/select.jsx
@@ -1,12 +1,12 @@
 const Select = ({ name, label, options, error, ...rest }) => {
   return (
-    <div className="row g-2 mb-3 align-items-center">
-      <div className="col-4 col-sm-2 text-end">
+    <div className="row mb-3 align-items-center">
+      <div className="col text-end">
         <label htmlFor={name} className="col-form-label text-success fw-bold fs-5">
           {label}
         </label>
       </div>
-      <div className="col-6 col-sm-4">
+      <div className="col ">
         <select className="form-select" name={name} id={name} {...rest}>
           <option value="" />
           {options.map((option) => (
